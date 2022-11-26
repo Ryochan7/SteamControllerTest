c2f891bbfde7f3f459151a1f25a0b2af9fb5e36d
Travis Nickles <ryochan7@protonmail.com>
2022-11-26 11:29:37 -0600

    Added placeholder for device specific profile settings

---
+++ SteamControllerTest/ProfileDeviceSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamControllerTest
+{
+    public abstract class ProfileDeviceSettings
+    {
+    }
+
+    public class SteamControllerProfileDeviceSettings : ProfileDeviceSettings
+    {
+        public SteamControllerProfileDeviceSettings(Profile tempProfile)
+        {
+        }
+    }
+
+    public class DS4ProfileDeviceSettings : ProfileDeviceSettings
+    {
+        public DS4ProfileDeviceSettings(Profile tempProfile)
+        {
+        }
+    }
+}
---
+++ SteamControllerTest/ProfileDeviceSettingsFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamControllerTest
+{
+    public static class ProfileDeviceSettingsFactory
+    {
+        public static ProfileDeviceSettings CreateDeviceSettings(InputDeviceType deviceType,
+            Profile tempProfile)
+        {
+            ProfileDeviceSettings result = null;
+            switch(deviceType)
+            {
+                case InputDeviceType.SteamController:
+                    {
+                        result = new SteamControllerProfileDeviceSettings(tempProfile);
+                    }
+
+                    break;
+                case InputDeviceType.DS4:
+                    {
+                        result = new DS4ProfileDeviceSettings(tempProfile);
+                    }
+
+                    break;
+                default: break;
+            }
+            return result;
+        }
+    }
+}
