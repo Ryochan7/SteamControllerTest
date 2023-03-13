5e3f7d163d66c9d4ccf103989b6d022130d89b31
Travis Nickles <ryochan7@protonmail.com>
2023-03-12 23:29:05 -0500

    Exposed End Delay for ReleaseFunc in GUI

--- SteamControllerTest/ViewModels/ReleaseFuncPropViewModel.cs
+++ SteamControllerTest/ViewModels/ReleaseFuncPropViewModel.cs
@@ -48,6 +48,20 @@ namespace SteamControllerTest.ViewModels
         }
         public event EventHandler DurationMsChanged;
 
+        public string DelayDurationMs
+        {
+            get => func.DelayDurationMs.ToString();
+            set
+            {
+                if (int.TryParse(value, out int temp))
+                {
+                    func.DurationMs = temp;
+                    DelayDurationMsChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+        public event EventHandler DelayDurationMsChanged;
+
         public bool Interruptable
         {
             get => func.interruptable;
